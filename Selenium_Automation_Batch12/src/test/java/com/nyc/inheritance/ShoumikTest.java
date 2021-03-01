dc4cecb50611b89e484b63b791d95e86a952cd9c
zshou <zshou@DESKTOP-EVDEU37>
2021-02-28 20:22:04 -0500

    Commit by Shoumik

---
+++ Selenium_Automation_Batch12/src/test/java/com/nyc/inheritance/ShoumikTest.java
@@ -0,0 +1,17 @@
+package com.nyc.inheritance;
+
+public class ShoumikTest {
+
+	public void getMath() {
+		int x = 4;
+		int y = 3;
+
+		System.out.println(x + y);
+
+	}
+
+	public static void main(String[] args) {
+		ShoumikTest obj = new ShoumikTest();
+		obj.getMath();
+	}
+}
